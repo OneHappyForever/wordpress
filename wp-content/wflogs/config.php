499d67596395d60f894fc6918b267bc9df676a2d
Josh <Josh.tesol@gmail.com>
2021-03-21 03:56:39 +0800

    2021年 03月 21日 星期日 03:54:17 CST

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1616867914;s:7:"authKey";s:64:"W(stK4Y)[<XSR+T@zSmG.YQvU_,~/)DC~2*SX%M7Y:JbJ6)ej- $Pt|uwp-C>bl,";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:49;}
